7a71803d82c7a97a1a608e61226c69f5b555a73d
Atharv <atharvs.twm@gmail.com>
2020-06-11 16:17:48 +0530

    Added proportional controller

--- controller.py
+++ controller.py
@@ -4,34 +4,39 @@
 from __future__ import print_function
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Twist, TwistStamped
 from mavros_msgs.msg import State
-from mavros_msgs.srv import CommandBool, SetMode 
+from mavros_msgs.srv import CommandBool, SetMode
 
-class DroneController:
+import utils
+
+class DroneController(object):
     """
     Simple drone controller class
     """
-    def __init__(self):
+    def __init__(self, rate=20):
 
-        # To keep track of current state and pose
+        # Rate has to be greater than 2Hz for px4
+        self.rate = rospy.Rate(rate)
+
         self.current_state = State()
         self.pose = PoseStamped()
+        self.setpoint_pos = PoseStamped()
+        self.vel = TwistStamped()
+        self.setpoint_vel = Twist()
 
-        # Subscribe to current state of the drone
         self.state_sub = rospy.Subscriber("mavros/state", State, callback=self.state_cb)
-        
-        # Subscribe to current position of the drone
         self.pose_sub = rospy.Subscriber("mavros/local_position/pose", PoseStamped, callback=self.pose_cb)
+        self.vel_sub = rospy.Subscriber("mavros/local_position/velocity", TwistStamped, callback=self.vel_cb)
 
-        # Publishisher to set local position to move to
-        self.local_pos_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
-        
-        # Service client for arming the drone
-        self.arm_service = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
-        
-        # Service client for setting the mode of the drone
-        self.set_mode_service = rospy.ServiceProxy("mavros/set_mode", SetMode)
+        self.setpoint_pos_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
+        self.setpoint_vel_pub = rospy.Publisher("mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=10)
+
+        rospy.wait_for_service('mavros/cmd/arming')
+        self.arm_service = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+        rospy.wait_for_service('mavros/set_mode')
+        self.set_mode_service = rospy.ServiceProxy('mavros/set_mode', SetMode)
+ 
 
     def state_cb(self, data):
         """
@@ -45,6 +50,13 @@ class DroneController:
         """
         self.pose = data
 
+    def vel_cb(self, data):
+        """
+        Callback for pose subscriber. Recieves and stores current pose.
+        """
+        self.vel = data
+
+
     def init_stream(self, rate, n=100):
         """
         Stream setpoint for specified number of times.
@@ -52,7 +64,7 @@ class DroneController:
         """
         for _ in range(n):
             pose = PoseStamped()
-            self.local_pos_pub.publish(pose)
+            self.setpoint_pos_pub.publish(pose)
             rate.sleep()
 
     def arm(self):
@@ -60,44 +72,96 @@ class DroneController:
         Arms the drone through arming service.
         """
         result = self.arm_service(True)
-        print("Arming Drone: ", result)
+        if result.success:
+            rospy.loginfo("Arming successful")       
+        else:
+            rospy.loginfo("Arming unsuccessful")
 
     def disarm(self):
         """
         Disarms the drone through arming service
         """
         result = self.arm_service(True)
-        print("Disarming Drone: ", result)
-
-    def set_mode(self, **kwargs):
-        """
-        Sets the mode of the drone through set mode service.
-        """
-        result = self.set_mode_service(**kwargs)
-        # print("Setting mode to ", str(kwargs), ": ", result)
-
-    def set_pose(self, position):
-        """
-        Sets position of drone by publishing local position to move to,
-        to the setpoint_position/local topic.
-        """
-        x, y, z = position
-
-        pose = PoseStamped()
-        pose.pose.position.x = x
-        pose.pose.position.y = y
-        pose.pose.position.z = z
-
-        self.local_pos_pub.publish(pose)
+        if result.success:
+            rospy.loginfo("Disarming successful")       
+        else:
+            rospy.loginfo("Disarming unsuccessful")
 
     def land(self):
         """
         Lands drone and disarms it
         """
-        result = self.set_mode(custom_mode="AUTO.LAND")
-        print("Landing drone ")
-        if self.pose.pose.position.z < 0.1:
-            self.disarm()
+        rospy.loginfo("Landing")
+        self.set_mode("AUTO.LAND")
+
+    def set_mode(self, mode):
+        """
+        Sets the mode of the drone through set mode service.
+        """
+        result = self.set_mode_service(custom_mode=mode)
+        if result.mode_sent:
+            rospy.logdebug("Setting mode to %s successful", mode)       
+        else:
+            rospy.logdebug("Setting mode to %s unsuccessful", mode)
+
+    def _publish_pos(self):
+        self.setpoint_pos_pub.publish(self.setpoint_pos)
+
+    def set_pos(self, x=None, y=None, z=None):
+        """
+        Sets position of drone by publishing local position to move to,
+        to the setpoint_position/local topic.
+        """
+
+        if x is not None: self.setpoint_pos.pose.position.x = x
+        if y is not None: self.setpoint_pos.pose.position.y = y
+        if z is not None: self.setpoint_pos.pose.position.z = z
+
+        self._publish_pos()
+
+    def _publish_vel(self):
+        self.setpoint_vel_pub.publish(self.setpoint_vel)
+
+    def set_vel(self, vx=None, vy=None, vz=None, az=None):
+        """
+        Sets position of drone by publishing local position to move to,
+        to the setpoint_position/local topic.
+        """
+        if vx is not None: self.setpoint_vel.linear.x = vx
+        if vy is not None: self.setpoint_vel.linear.y = vy
+        if vz is not None: self.setpoint_vel.linear.z = vz
+        if az is not None: self.setpoint_vel.angular.z = az
+        
+        self._publish_vel()
+
+    def takeoff(self, h=1):
+        """
+        Prepare drone and takeoff
+        """
+        # Wait till connection established between MAVROS and px4 autopilot
+        rospy.loginfo("Connecting to autopilot")
+        while not self.current_state.connected:
+            self.rate.sleep()
+
+        # Arms the drone
+        self.arm()
+
+        # Need to start streaming setpoints before entering OFFBOARD mode
+        self.init_stream(self.rate)
+
+        rospy.loginfo("Taking off")
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        z = h
+
+        while utils.dist(utils.unwrap_pose(self.pose.pose)[0], (x, y, z)) > 0.1:
+            # Attempt to enter into OFFBOARD mode
+            if self.current_state.mode != "OFFBOARD":
+                self.set_mode("OFFBOARD")
+        
+            # Set position to move to
+            self.set_pos(x, y, z)
+            self.rate.sleep()
 
 
 if __name__ == "__main__":
@@ -107,35 +171,13 @@ if __name__ == "__main__":
     # Initialises controller object with its publishers, subscribers and clients
     drone = DroneController()
 
-    # Rate has to be greater than 2Hz for px4
-    rate = rospy.Rate(20)
-
-    # Wait till connection established between MAVROS and px4 autopilot
-    print("Connecting to autopilot: ", end=' ')
-    while not rospy.is_shutdown() and not drone.current_state.connected:
-        rate.sleep()
-    print("connected")
-
-    # Arms the drone
-    drone.arm()
-
-    # Need to start streaming setpoints before entering OFFBOARD mode
-    drone.init_stream(rate)
-
     try:
-        # Main loop
-        while not rospy.is_shutdown():
+        # Drone takes off and lands
+        drone.takeoff(2)
+        drone.land()
 
-            # Attempt to enter into OFFBOARD mode
-            if drone.current_state.mode != "OFFBOARD":
-                drone.set_mode(custom_mode="OFFBOARD")
-            
-            # Set position to move to
-            drone.set_pose(position=(0, 0, 2))
-            rate.sleep()
-
-    # Land drone if keyboard interrupt is recieved        
     except KeyboardInterrupt:
+        # Land drone if keyboard interrupt is recieved        
         print(" <-- In keyboard interrupt ...")
         try:
             drone.land()
